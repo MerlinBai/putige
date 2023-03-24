67d9a681548918731272723369350146fa51bbd8
BAI15 <15442866802qq.com>
2023-03-24 19:42:45 +0800

    使用springcache，缓存验证码菜品

--- src/main/java/com/itheima/controller/SetmealController.java
+++ src/main/java/com/itheima/controller/SetmealController.java
@@ -12,6 +12,8 @@ import com.itheima.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -39,12 +41,12 @@ public class SetmealController {
 
 
     @PostMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> save(@RequestBody SetmealDto setmealDto){
 
     setmealService.saveWithDish(setmealDto);
         return R.success("新增套餐成功");
         }
-
 
 
     @GetMapping("/page")
@@ -94,6 +96,7 @@ public class SetmealController {
      * @return
      */
     @DeleteMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
         public R<String> delete(@RequestParam List<Long> ids){
          setmealService.removeWithDish(ids);
         return R.success("套餐数据删除成功");
@@ -102,6 +105,7 @@ public class SetmealController {
 
 
         @GetMapping("/list")
+        @Cacheable(value = "setmealCache",key = "#setmeal.categoryId+'_'+#setmeal.status")
         public R<List<Setmeal>> list(Setmeal setmeal){
             LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
             queryWrapper.eq(setmeal.getCategoryId()!=null,Setmeal::getCategoryId,setmeal.getCategoryId());
